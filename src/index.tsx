4ddfa932d129e6900fd484a6b5a8d1037a281cec
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-26 09:27:48 +0300

    replace ReactDOM.render with createRoot according to React 18 demand

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+
+const container = document.getElementById('root')
+const root = createRoot(container!)
 root.render(
     <App />,
 );

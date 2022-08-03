2a936f49cb0616cde2f7a3aca2ac2fca199710a2
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-03 09:36:54 +0300

    add new stories of Clock component

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -7,6 +7,14 @@ export default {
     component: Clock
 } as ComponentMeta<typeof Clock>
 
-const Template: ComponentStory<typeof Clock> = () => <Clock/>;
-export const ClockExample = Template.bind({})
+const Template: ComponentStory<typeof Clock> = (args) => <Clock{...args}/>;
 
+export const AnalogueClockExample = Template.bind({})
+AnalogueClockExample.args = {
+    analogueClockMode : true
+}
+
+export const DigitalClockExample = Template.bind({})
+DigitalClockExample.args = {
+    analogueClockMode : false
+}
